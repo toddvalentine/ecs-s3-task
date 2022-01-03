dc440d349c81e563596b5d306fb3ae7ba4c73e8f
toddvalentine-fc <todd.valentine@flipcause.com>
2022-01-03 16:18:33 -0600

    set up task roles

--- lib/vtypeio-ecs-s3-task-stack.ts
+++ lib/vtypeio-ecs-s3-task-stack.ts
@@ -1,6 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class VtypeioEcsS3TaskStack extends Stack {
@@ -15,10 +17,26 @@ export class VtypeioEcsS3TaskStack extends Stack {
 
     const vpc = new ec2.Vpc(this, 'taskVpc', {
       maxAzs: 2,
-      // TODO: review nat gateway/private subnets
+      // TODO: review if we need nat gateway/private subnets
       natGateways: 0
     });
 
-    
+    const cluster = new ecs.Cluster(this, 'ecsCluster', {
+      vpc: vpc
+    });
+
+    const ter = new iam.Role(this, 'taskExecutionRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com')
+    });
+
+    const tr = new iam.Role(this, 'taskRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com')
+    });
+    tr.addToPolicy(new iam.PolicyStatement({
+      actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+      resources: ['*']
+    }));
+    sourceBucket.grantRead(tr)
+
   }
 }
