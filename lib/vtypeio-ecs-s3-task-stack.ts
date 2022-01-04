b44842a2203ed05a262881bceb3178f042092ea8
toddvalentine-fc <todd.valentine@flipcause.com>
2022-01-04 16:43:30 -0600

    begin task runner

--- lib/vtypeio-ecs-s3-task-stack.ts
+++ lib/vtypeio-ecs-s3-task-stack.ts
@@ -3,6 +3,8 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Function } from '../myconstructs/function';
 import { Construct } from 'constructs';
 
 export class VtypeioEcsS3TaskStack extends Stack {
@@ -42,5 +44,48 @@ export class VtypeioEcsS3TaskStack extends Stack {
     }));
     sourceBucket.grantRead(tr)
 
+    const td = new ecs.FargateTaskDefinition(this, 'taskDefinition', {
+      memoryLimitMiB: 512,
+      cpu: 256,
+      executionRole: ter,
+      taskRole: tr,
+    })
+    const taskContainer = td.addContainer('taskContainer', {
+      image: ecs.AssetImage.fromAsset('../task'),
+      logging: ecs.LogDriver.awsLogs({
+        streamPrefix: 'task',
+      })
+    })
+    
+    const taskStarterRole = new iam.Role(this, 'taskStarterRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+    })
+    taskStarterRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'))
+    taskStarterRole.addToPolicy(new iam.PolicyStatement({
+      actions: ['ecs:RunTask'],
+      resources: [cluster.clusterArn, td.taskDefinitionArn]
+    }))
+    td.executionRole?.grantPassRole(taskStarterRole)
+    td.taskRole.grantPassRole(taskStarterRole)
+
+    const taskStarter = new Function(this, 'taskStarter', {
+      entry: '../taskrunner',
+      environment: { 
+        "CLUSTER_ARN": cluster.clusterArn, 
+        "CONTAINER_NAME": taskContainer.containerName,
+        "TASK_DEFINITION_ARN": td.taskDefinitionArn,
+        "SUBNETS": getSubnetIDs(vpc.publicSubnets).join(),
+        "S3_BUCKET": sourceBucket.bucketName,
+      }
+    })
+
   }
+}
+
+function getSubnetIDs(subnets: ec2.ISubnet[] ): string[] {
+    var ids: string[] = [];
+    for (let i = 0; i < subnets.length; i++) {
+      ids.push(subnets[i].subnetId)
+    }
+    return ids
 }
