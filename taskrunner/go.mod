6bb396940b660fc9c7eba909cb3dd56204ca3869
toddvalentine-fc <todd.valentine@flipcause.com>
2022-01-05 15:42:01 -0600

    more taskrunner

--- taskrunner/go.mod
+++ taskrunner/go.mod
@@ -4,11 +4,12 @@ go 1.17
 
 require (
 	github.com/aws/aws-lambda-go v1.27.1
+	github.com/aws/aws-sdk-go v1.42.28
 	go.uber.org/zap v1.20.0
 )
 
 require (
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 )
--- taskrunner/main.go
+++ taskrunner/main.go
@@ -7,6 +7,9 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecs"
 	"go.uber.org/zap"
 )
 
@@ -64,5 +67,45 @@ func main() {
 }
 
 func handler(ctx context.Context, s3Event events.S3Event) error {
+	logger.Info("Starting...")
+	sess, err := session.NewSession()
+	if err != nil {
+		logger.Fatal("Unable to create session, exiting...")
+	}
+	svc := ecs.New(sess)
+	for i, record := range s3Event.Records {
+		s3 := record.S3
+		logger.Info("Processing File", zap.Int("index", i), zap.Int("count", len(s3Event.Records)), zap.String("bucketName", s3.Bucket.Name), zap.String("objectKey", s3.Object.Key))
+		input := &ecs.RunTaskInput{
+			Cluster:        &c.ClusterARN,
+			TaskDefinition: &c.TaskDefinitionARN,
+			NetworkConfiguration: &ecs.NetworkConfiguration{
+				AwsvpcConfiguration: &ecs.AwsVpcConfiguration{
+					// Set to true if in the public subnet so that the container can be downloaded.
+					AssignPublicIp: aws.String(ecs.AssignPublicIpEnabled),
+					Subnets:        aws.StringSlice(c.Subnets),
+				},
+			},
+			Overrides: &ecs.TaskOverride{
+				ContainerOverrides: []*ecs.ContainerOverride{{
+					Name: &c.ContainerName,
+					Environment: []*ecs.KeyValuePair{
+						{Name: aws.String("S3_KEY"), Value: &s3.Object.Key},
+						{Name: aws.String("S3_BUCKET"), Value: &c.S3Bucket},
+					},
+				}},
+			},
+			LaunchType: aws.String(ecs.LaunchTypeFargate),
+		}
+
+		res, err := svc.RunTask(input)
+		if err != nil {
+			logger.Error("Failed to run task", zap.Error(err))
+			return err
+		}
+		for _, task := range res.Tasks {
+			logger.Info("Started task", zap.String("taskId", *task.TaskArn))
+		}
+	}
 	return nil
 }
