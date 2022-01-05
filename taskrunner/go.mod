8949fa17f202a8262e79466b5a559794ba2d8564
toddvalentine-fc <todd.valentine@flipcause.com>
2022-01-05 15:08:46 -0600

    begin taskrunner

---
+++ taskrunner/go.mod
@@ -0,0 +1,14 @@
+module taskrunner
+
+go 1.17
+
+require (
+	github.com/aws/aws-lambda-go v1.27.1
+	go.uber.org/zap v1.20.0
+)
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
--- taskrunner/main.go
+++ taskrunner/main.go
@@ -1,5 +1,68 @@
 package main
 
-func main() {
+import (
+	"context"
+	"os"
+	"strings"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"go.uber.org/zap"
+)
+
+type config struct {
+	ClusterARN        string
+	ContainerName     string
+	TaskDefinitionARN string
+	Subnets           []string
+	S3Bucket          string
+	IsValid           bool
+}
+
+var logger *zap.Logger
+var c config
+
+func main() {
+	var err error
+	logger, err = zap.NewProduction()
+	if err != nil {
+		panic("failed to create logger: " + err.Error())
+	}
+
+	c.IsValid = true
+	c.ClusterARN = os.Getenv("CLUSTER_ARN")
+	if c.ClusterARN == "" {
+		logger.Error("CLUSTER_ARN not set")
+		c.IsValid = false
+	}
+	c.TaskDefinitionARN = os.Getenv("TASK_DEFINITION_ARN")
+	if c.TaskDefinitionARN == "" {
+		logger.Error("TASK_DEFINITION_ARN not set")
+		c.IsValid = false
+	}
+	c.ContainerName = os.Getenv("CONTAINER_NAME")
+	if c.ContainerName == "" {
+		logger.Error("CONTAINER_NAME not set")
+		c.IsValid = false
+	}
+	subnets := os.Getenv("SUBNETS")
+	if subnets == "" {
+		logger.Error("SUBNETS not set")
+		c.IsValid = false
+	}
+	c.Subnets = strings.Split(subnets, ",")
+	c.S3Bucket = os.Getenv("S3_BUCKET")
+	if c.S3Bucket == "" {
+		logger.Error("S3_BUCKET not set")
+		c.IsValid = false
+	}
+	if !c.IsValid {
+		logger.Fatal("Invalid configuration, exiting...")
+	}
+
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, s3Event events.S3Event) error {
+	return nil
 }
