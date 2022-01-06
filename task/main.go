abd27686d37aa59edb1f26715cf88ad60da4dbb2
toddvalentine-fc <todd.valentine@flipcause.com>
2022-01-06 15:56:00 -0600

    log bucket and key names

--- task/main.go
+++ task/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("OK, I ran.")
+	fmt.Printf("Bucket Name: %s\n", os.Getenv("S3_BUCKET"))
+	fmt.Printf("Object Key: %s\n", os.Getenv("S3_KEY"))
 }
